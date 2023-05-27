eabf1e5f492b0c24358b062d810ccc9342fcd3a0
Md Asif Raza <mdasifr28@gmail.com>
2023-05-28 00:56:54 +0530

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,162 @@
-import Image from 'next/image'
+import React from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <React.Fragment>
+      <div className="flex h-full min-h-screen flex-col items-center bg-[#0E1117] px-4 pb-20 text-neutral-200 sm:px-10">
+        <div className="mt-10 flex flex-col items-center justify-center sm:mt-20">
+          <div className="text-4xl font-bold">AI Code Translator</div>
+        </div>
+        <div className="mt-6 text-center text-sm">
+          <input
+            className="mt-1 h-[24px] w-[280px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            type="password"
+            placeholder="OpenAI API Key"
+          />
+        </div>
+        <div className="mt-2 flex items-center space-x-2">
+          <select className="h-[40px] w-[140px] rounded-md bg-[#1F2937] px-4 py-2 text-neutral-200">
+            <option value="gpt-3.5-turbo">
+              GPT-3.5
+            </option>
+            <option value="gpt-4">GPT-4</option>
+          </select>
+          <button className="w-[140px] cursor-pointer rounded-md bg-violet-500 px-4 py-2 font-bold hover:bg-violet-600 active:bg-violet-700">
+            Translate
+          </button>
+        </div>
+        <div className="mt-2 text-center text-xs">
+          Enter some code and click "Translate"
+        </div>
+        <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
+          <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
+            <div className="text-center text-xl font-bold">Input</div>
+            <select className="w-full rounded-md bg-[#1F2937] px-4 py-2 text-neutral-200">
+              <option value="Assembly Language">Assembly Language</option>
+              <option value="Bash">Bash</option>
+              <option value="C">C</option>
+              <option value="C#">C#</option>
+              <option value="C++">C++</option>
+              <option value="Clojure">Clojure</option>
+              <option value="COBOL">COBOL</option>
+              <option value="CoffeeScript">CoffeeScript</option>
+              <option value="CSS">CSS</option>
+              <option value="Dart">Dart</option>
+              <option value="Elixir">Elixir</option>
+              <option value="Fortran">Fortran</option>
+              <option value="Go">Go</option>
+              <option value="Groovy">Groovy</option>
+              <option value="Haskell">Haskell</option>
+              <option value="HTML">HTML</option>
+              <option value="Java">Java</option>
+              <option value="JavaScript">JavaScript</option>
+              <option value="JSX">JSX</option>
+              <option value="Julia">Julia</option>
+              <option value="Kotlin">Kotlin</option>
+              <option value="Lisp">Lisp</option>
+              <option value="Lua">Lua</option>
+              <option value="Matlab">Matlab</option>
+              <option value="Natural Language">Natural Language</option>
+              <option value="NoSQL">NoSQL</option>
+              <option value="Objective-C">Objective-C</option>
+              <option value="Pascal">Pascal</option>
+              <option value="Perl">Perl</option>
+              <option value="PHP">PHP</option>
+              <option value="PL/SQL">PL/SQL</option>
+              <option value="Powershell">Powershell</option>
+              <option value="Python">Python</option>
+              <option value="R">R</option>
+              <option value="Racket">Racket</option>
+              <option value="Ruby">Ruby</option>
+              <option value="Rust">Rust</option>
+              <option value="SAS">SAS</option>
+              <option value="Scala">Scala</option>
+              <option value="SQL">SQL</option>
+              <option value="Swift">Swift</option>
+              <option value="SwiftUI">SwiftUI</option>
+              <option value="TSX">TSX</option>
+              <option value="TypeScript">TypeScript</option>
+              <option value="Visual Basic .NET">Visual Basic .NET</option>
+              <option value="Vue">Vue</option>
+            </select>
+            <div className="relative">
+              <button className="absolute right-0 top-0 z-10 rounded bg-[#1A1B26] p-1 text-xs text-white hover:bg-[#2D2E3A] active:bg-[#2D2E3A]">
+                Copy
+              </button>
+              <textarea
+                name="output"
+                id=""
+                cols={30}
+                rows={10}
+              >
+              </textarea>
+            </div>
+          </div>
+          <div className="mt-8 flex h-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
+            <div className="text-center text-xl font-bold">Output</div>
+            <select className="w-full rounded-md bg-[#1F2937] px-4 py-2 text-neutral-200">
+              <option value="Assembly Language">Assembly Language</option>
+              <option value="Bash">Bash</option>
+              <option value="C">C</option>
+              <option value="C#">C#</option>
+              <option value="C++">C++</option>
+              <option value="Clojure">Clojure</option>
+              <option value="COBOL">COBOL</option>
+              <option value="CoffeeScript">CoffeeScript</option>
+              <option value="CSS">CSS</option>
+              <option value="Dart">Dart</option>
+              <option value="Elixir">Elixir</option>
+              <option value="Fortran">Fortran</option>
+              <option value="Go">Go</option>
+              <option value="Groovy">Groovy</option>
+              <option value="Haskell">Haskell</option>
+              <option value="HTML">HTML</option>
+              <option value="Java">Java</option>
+              <option value="JavaScript">JavaScript</option>
+              <option value="JSX">JSX</option>
+              <option value="Julia">Julia</option>
+              <option value="Kotlin">Kotlin</option>
+              <option value="Lisp">Lisp</option>
+              <option value="Lua">Lua</option>
+              <option value="Matlab">Matlab</option>
+              <option value="Natural Language">Natural Language</option>
+              <option value="NoSQL">NoSQL</option>
+              <option value="Objective-C">Objective-C</option>
+              <option value="Pascal">Pascal</option>
+              <option value="Perl">Perl</option>
+              <option value="PHP">PHP</option>
+              <option value="PL/SQL">PL/SQL</option>
+              <option value="Powershell">Powershell</option>
+              <option value="Python">Python</option>
+              <option value="R">R</option>
+              <option value="Racket">Racket</option>
+              <option value="Ruby">Ruby</option>
+              <option value="Rust">Rust</option>
+              <option value="SAS">SAS</option>
+              <option value="Scala">Scala</option>
+              <option value="SQL">SQL</option>
+              <option value="Swift">Swift</option>
+              <option value="SwiftUI">SwiftUI</option>
+              <option value="TSX">TSX</option>
+              <option value="TypeScript">TypeScript</option>
+              <option value="Visual Basic .NET">Visual Basic .NET</option>
+              <option value="Vue">Vue</option>
+            </select>
+            <div className="relative">
+              <button className="absolute right-0 top-0 z-10 rounded bg-[#1A1B26] p-1 text-xs text-white hover:bg-[#2D2E3A] active:bg-[#2D2E3A]">
+                Copy
+              </button>
+              <textarea
+                name="output"
+                id=""
+                cols={30}
+                rows={10}
+              >
+              </textarea>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </React.Fragment>
   )
 }
